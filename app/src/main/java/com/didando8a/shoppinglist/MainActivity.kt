fd56d97cc0627b58340ed820703d6af2ad08b9b1
Diego Daniel Dorado Ochoa <didando8a@gmail.com>
2024-03-01 12:42:34 +0100

    Add rememberSavable

--- app/src/main/java/com/didando8a/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/didando8a/shoppinglist/MainActivity.kt
@@ -6,17 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.didando8a.shoppinglist.ui.theme.ShoppingListTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.saveable.rememberSaveable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,12 +29,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(
+fun OnboardingScreen(
     modifier: Modifier = Modifier,
-    names:List<String> = listOf("Consue", "Diego")
+    onContinueClicked: () -> Unit,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-    Column(modifier = Modifier.padding(vertical = 10.dp)) {
-        for (name in names) {
+        Text(text = "Welcome to Dani's lab")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    Greetings()
+}
+
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names:List<String> = List(30) {"$it"}
+) {
+    LazyColumn(modifier = Modifier.padding(vertical = 10.dp)) {
+        items (items =  names) { name ->
             Greeting(name = name)
         }
     }
@@ -48,10 +74,28 @@ fun MyApp(
 //    }
 }
 
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier, ) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember {
+    val expanded = rememberSaveable {
         mutableStateOf(false)
+    }
+
+    var counter by rememberSaveable {
+        mutableIntStateOf(0)
     }
 
     Surface(color = MaterialTheme.colorScheme.primary, modifier = modifier.padding(vertical = 4.dp)) {
@@ -63,14 +107,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 .fillMaxWidth(0.5f)
                 .padding(vertical = 10.dp), text = name)
             ElevatedButton(
-                onClick = { expanded.value = !expanded.value }) {
-                val messageString: String = if(expanded.value) "Show less" else "Show more"
+                onClick = {
+                    expanded.value = !expanded.value
+                    counter = counter.plus(1)
+                }) {
+                val messageString: String = if(expanded.value) "Show more ${counter}" else "Show less ${counter}"
                 Text(modifier = modifier
                     .fillMaxWidth(0.5f)
                     , text = messageString)
             }
         }
     }
+}
+
+fun messageButtonPrinter(counter: MutableIntState): String
+{
+    return "The button has been clicked ${counter.intValue} times"
 }
 
 @Preview(showBackground = true)
